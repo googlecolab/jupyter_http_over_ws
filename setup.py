75fd8d98f0e3e596100b3e622e173ac27e6bce81
Craig Citro <craigcitro@google.com>
2020-03-12 09:01:58 -0700

    Internal update.
    PiperOrigin-RevId: 300558931

--- setup.py
+++ setup.py
@@ -13,24 +13,13 @@
 # limitations under the License.
 """Configuration for the jupyter_http_over_ws package."""
 
-import sys
-
 from setuptools import find_packages
 from setuptools import setup
-
-DEPENDENCIES = (
-    'notebook>=5.0',
-    'six>=1.6.0',
-    'tornado>=4.5',
-)
-
-if sys.version_info[0] == 2:
-  DEPENDENCIES += ('enum34~=1.1',)
 
 setup(
     name='jupyter_http_over_ws',
     # LINT.IfChange(handler_version)
-    version='0.0.7',
+    version='0.0.8',
     # LINT.ThenChange(../handlers.py:handler_version)
     author='Google Colaboratory team',
     author_email='colaboratory-team@google.com',
@@ -38,7 +27,12 @@ setup(
     long_description='Jupyter support for HTTP-over-ws',
     url='https://github.com/googlecolab/jupyter_http_over_ws',
     packages=find_packages(exclude=('tests*',)),
-    install_requires=DEPENDENCIES,
+    install_requires=(
+        'enum34~=1.1;python_version<"3.4"',
+        'notebook>=5.0',
+        'six>=1.6.0',
+        'tornado>=4.5',
+    ),
     license='Apache 2.0',
     keywords='google colab ipython jupyter http_over_ws',
     classifiers=(
